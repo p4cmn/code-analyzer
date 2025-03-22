f5f77496448959229cdb4288af169468f5556736
edgerunner <i.am.artem.derkach@gmail.com>
2025-03-22 19:04:21 +0300

    feat: refactor symbol table
    - add documentation
    - refactor variable names

--- SymbolTable/SymbolTable.cpp
+++ SymbolTable/SymbolTable.cpp
@@ -1,18 +1,24 @@
 #include "SymbolTable.h"
 
-SymbolTable::SymbolTable() : nextId(0) {}
+SymbolTable::SymbolTable()
+        : m_symbolMap()
+        , m_nextId(0)
+{}
 
 int SymbolTable::addSymbol(const std::string& symbol) {
-  auto it = table.find(symbol);
-  if (it != table.end()) {
+  auto it = m_symbolMap.find(symbol);
+  if (it != m_symbolMap.end()) {
     return it->second;
   }
-  int id = nextId++;
-  table[symbol] = id;
-  return id;
+  int newId = m_nextId++;
+  m_symbolMap[symbol] = newId;
+  return newId;
 }
 
 int SymbolTable::lookup(const std::string& symbol) const {
-  auto it = table.find(symbol);
-  return (it != table.end()) ? it->second : -1;
+  auto it = m_symbolMap.find(symbol);
+  if (it != m_symbolMap.end()) {
+    return it->second;
+  }
+  return -1;
 }
